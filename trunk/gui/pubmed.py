b140b60b57a7dcc98e678157d69c3676d6c9e049
linusben <linusben@821f101f-3830-0410-9a62-89dde35e5262>
2007-06-14 10:18:12 +0000

    - Changing names

--- trunk/gui/pubmed.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python 
-from lib.pubmed import *;
-
-term = 'elegans AND 2006[dp]';
-retmax = '50';
-
-pm = pubmed_search(term, retmax);
-for article in pm.articles:
-  print article.authors,article.title;
