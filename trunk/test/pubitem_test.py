a4a99b19c139d5cfc2446f11f5794007e21ac895
linusben <linusben@821f101f-3830-0410-9a62-89dde35e5262>
2007-06-15 16:23:36 +0000

    - Add example code to insert pubitem

--- trunk/test/pubitem_test.py
+++ trunk/test/pubitem_test.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python
+import yaml
 import sys
 sys.path.append('../libpy/')
 from model_pubitem import PubItem
 
-pm = PubItem(authors='Harris HS,Kim SK', pub_year=2004, page='2-4')
-print pm.get_nickname_base()
+data1 = yaml.load( file('./pubitem_example1.yaml','r') )
+pm1 = PubItem(title=data1['title'], authors=data1['authors'],
+              journal=data1['journal'], volume=data1['volume'],
+              page=data1['page'], pub_year=data1['pub_year'],
+              pub_type=data1['pub_type'])
+
+pm1.insert()
+print pm1.nickname
+#pm = PubItem(authors='Harris HS,Kim SK', pub_year=2004, page='2-4')
+#print pm.get_nickname_base()
--- trunk/test/refresh.sh
+++ trunk/test/refresh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm ../db.test/pubshelf.db
-sqlite3 ../db.test/pubshelf.db < ../etc/pubshelf_sqlite3.sql
+sqlite3 ../db.test/pubshelf.db < ../conf/schema.sqlite3.sql
 #rm -rf ../db.test/????
