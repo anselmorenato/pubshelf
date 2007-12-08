cc15548a5ea711b9d02a186505516590c46a2bfd
linusben <linusben@821f101f-3830-0410-9a62-89dde35e5262>
2007-12-08 07:36:27 +0000

    * gui/main_list.py  - exclude niftylist code from it (it did not work on Linux).  - just sort pubitem list on current main_list, when you click the column.

--- gui/main_list.py
+++ gui/main_list.py
@@ -1,59 +1,45 @@
 import wx
 from dialog_pubitem import PubShelfPubItemDialog
 from model_pubitem import PubItem
-from niftylist import NiftyVirtualList, NiftyListItem
 
 WIDTH_NICKNAME = 130
 WIDTH_YEAR = 70
 WIDTH_TITLE = 400
 
-#http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/426407
-
-class PubShelfItemList(NiftyVirtualList):
+class PubShelfItemList(wx.ListCtrl):
   def __init__(self, parent, id):
     window_style = wx.LC_REPORT
-    super(PubShelfItemList, self).__init__(parent, id, style=window_style | wx.LC_VIRTUAL )
+    super(PubShelfItemList, self).__init__(parent, id, style=window_style)
     
     self.InsertColumn(0, 'NickName', width=WIDTH_NICKNAME)
     self.InsertColumn(1, 'Year', width=WIDTH_YEAR)
     self.InsertColumn(2, 'Title', width=WIDTH_TITLE)
+    self.sort_type = 0 ## 0 : non-sort, 1: ascending, -1: descending
     
+    self.pubitems = []
+
     self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnListItemSelected)
     self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnPubItemActivated)
-    self.Bind(wx.EVT_LIST_COL_CLICK, self.OnColClick)
-
-    self.colsort = [0, 0, 0] # 0=ascending ready, 1=descending ready
-    self.colsortfunc = [
-                        lambda x, y: cmp(x.nickname, y.nickname), 
-                        lambda x, y: cmp(x.pub_year, y.pub_year),
-                        lambda x, y: cmp(x.title, y.title),
-                        lambda x, y: cmp(y.nickname, x.nickname),
-                        lambda x, y: cmp(y.pub_year, x.pub_year),
-                        lambda x, y: cmp(y.title, x.title)
-                       ]
+    self.Bind(wx.EVT_LIST_COL_CLICK, self.OnColumnClicked)
 
   def SetItemList(self, pubitems):
-    self.DeleteAllItems()
-    self.SetSort(lambda x, y: cmp(x.id, y.id)) # default sorting
-
+    self.pubitems = []
     for pubitem in pubitems:
-      item = self.InsertItem(data = pubitem)
-      item.SetText(pubitem.nickname, 0)
-      item.SetText(str(pubitem.pub_year), 1)
-      item.SetText(pubitem.title, 2)
-    
-    self.colsort = [0, 0, 0] # 0=ascending ready, 1=descending ready
+      self.pubitems.append( pubitem )
+    self.Refresh()
   
   def remove_by_pubitem_id(self, id):
-    self.SetFilter(lambda x: x.id == id)
-    self.DeleteItem(0)
-    self.SetFilter(lambda x: True)
+    idx = 0;
+    for pubitem in self.pubitems:
+      if(pubitem.id == id): 
+        del self.pubitems[idx]
+      idx += 1
 
-  def OnColClick(self, event):
-    col = event.m_col
-    self.SetSort(self.colsortfunc[self.colsort[col]*3+col])
-    self.colsort[col] = 1 - self.colsort[col]
-    
+  def Refresh(self):
+    self.DeleteAllItems()
+    for pubitem in self.pubitems:
+      self.Append([pubitem.nickname, pubitem.pub_year, pubitem.title])
+      
   def OnResize(self):
     if( self.GetSize().x > 0 ):
       WIDTH_TITLE = self.GetSize().x - WIDTH_NICKNAME - WIDTH_YEAR
@@ -75,3 +61,24 @@ class PubShelfItemList(NiftyVirtualList):
     dialog = PubShelfPubItemDialog(frame, -1)
     dialog.SetPubItem(pubitem)
     dialog.Show()
+  
+  def OnColumnClicked(self, event):
+    column = event.m_col
+    if( self.sort_type == 1 ):
+      self.sort_type = -1
+      if( column == 1 ):
+        self.pubitems.sort(lambda x, y: y.pub_year - x.pub_year)
+      elif( column == 2 ):
+        self.pubitems.sort(lambda x, y: cmp(y.title, x.title))
+      else:
+        self.pubitems.sort(lambda x, y: cmp(y.nickname, x.nickname))
+    else:
+      self.sort_type = 1
+      if( column == 1 ):
+        self.pubitems.sort(lambda x, y: x.pub_year - y.pub_year)
+      elif( column == 2 ):
+        self.pubitems.sort(lambda x, y: cmp(x.title, y.title))
+      else:
+        self.pubitems.sort(lambda x, y: cmp(x.nickname, y.nickname))
+
+    self.Refresh()
